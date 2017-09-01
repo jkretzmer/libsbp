406f6a40b2a8bce367d964f5a4c77f7e19996425
dzollo <dzollo@swift-nav.com>
2017-09-01 15:32:46 -0700

    trying to make it simpler; still some comma errors

--- generator/sbpg/targets/resources/sbp2json_template.c
+++ generator/sbpg/targets/resources/sbp2json_template.c
@@ -68,20 +68,25 @@ int (((m.identifier|convert)))_to_json_str( (((in_ptr_type))) * in, uint64_t max
 
   ((*- if m.sbp_id -*)) ((= If message print out framing items=))
   json_bufp += snprintf(out_str, json_end - json_bufp, "{\"msg_type\": %u, \"sender\":%u, \"length\":%u", msg_type, sender_id, msg_len);
+  ((*- else -*))
+  json_bufp += snprintf(out_str, json_end - json_bufp, "{");
   ((*- endif *)) 
    
   ((= Separate Print for each field=))
   ((*- for field in m.fields -*))
   ((= simple type field =))
+  ((*- if (not m.sbp_id and field != m.fields[0] ) -*))  ((= comma in between every field except first=))
+  json_bufp += snprintf(json_bufp, json_end - json_bufp, ",");
+  ((*- endif -*))
   ((*- if (field.type_id|is_simple) -*))
-  json_bufp += snprintf(json_bufp, json_end - json_bufp, ", \"(((field.identifier)))\": (((field|get_format_str)))", in->(((field.identifier))));
+  json_bufp += snprintf(json_bufp, json_end - json_bufp, "\"(((field.identifier)))\": (((field|get_format_str)))", in->(((field.identifier))));
   ((= Nested type field =))
   ((*- elif (field.type_id != "array" and not field.type_id|is_string) -*)) 
-  json_bufp += snprintf(json_bufp, json_end - json_bufp, ", \"(((field.identifier)))\":");
+  json_bufp += snprintf(json_bufp, json_end - json_bufp, "\"(((field.identifier)))\": ");
   json_bufp += (((field.type_id|convert)))_to_json_str(&in->(((field.identifier))), json_end - json_bufp, json_bufp);
   ((= fixed legth string (special type of fixed size array in our case) =))
   ((*- elif (field|mk_id|is_string) and (field.options.get('size', None)) -*))
-  json_bufp += snprintf(json_bufp, json_end - json_bufp, ", \"(((field.identifier)))\":\"");
+  json_bufp += snprintf(json_bufp, json_end - json_bufp, "\"(((field.identifier)))\": \"");
   for (int i=0; i < (((field.options.get('size').value))); i++) {
     unsigned char c = in->(((field.identifier)))[i];
     if (isprint(c) && c != '\\') {
@@ -92,20 +97,7 @@ int (((m.identifier|convert)))_to_json_str( (((in_ptr_type))) * in, uint64_t max
     }
   }
   json_bufp += snprintf(json_bufp, json_end - json_bufp, "\"");
-  ((= fixed size array =))
-  ((*- elif (field.type_id == "array" and field.options.get('size', None)) -*))
-  json_bufp += snprintf(json_bufp, json_end - json_bufp, ", \"(((field.identifier)))\": {");
-  ((=loop over fixed size array =))
-  for (int i=0; i < (((field.options.get('size').value))); i++) {
-  ((*- if (field|mk_id|is_simple) -*))
-  json_bufp += snprintf(json_bufp, json_end - json_bufp, "(((field|get_format_str)))", in->(((field.identifier)))[i]);
-  ((*- else -*))
-  json_bufp += (((field|mk_id)))_to_json_str(in->(((field.identifier))), json_end - json_bufp, json_bufp);
-  ((*- endif -*))
-    }
-  ((= check for no size which means  greedy array at end of message =))
-  ((*- elif not (field.options.get('size', None)) and field == m.fields[-1] -*))
-  ((*- if (field|mk_id|is_string) -*))
+  ((*- elif (field|mk_id|is_string and not (field.options.get('size', None))) -*))
   json_bufp += snprintf(json_bufp, json_end - json_bufp, ", \"(((field.identifier)))\":\"");
   for (int i=0; i < (uint8_t *) in + msg_len - (uint8_t *) &(in->(((field.identifier)))); i++) {
     unsigned char c = in->(((field.identifier)))[i];
@@ -116,7 +108,25 @@ int (((m.identifier|convert)))_to_json_str( (((in_ptr_type))) * in, uint64_t max
     json_bufp += snprintf(json_bufp, json_end - json_bufp, "\\\\u00%02x", c);
     }
   }
-  ((*- endif -*)) ((= end if string =))
+  json_bufp += snprintf(json_bufp, json_end - json_bufp, "\"");
+  ((*- elif (field.type_id == "array" ) -*))
+  json_bufp += snprintf(json_bufp, json_end - json_bufp, "\"(((field.identifier)))\": [");
+  ((*- if field.options.get('size', None) -*))
+  ((=loop over fixed size array =))
+
+  for (int i=0; i < (((field.options.get('size').value))); i++) {
+
+  ((*- elif field == m.fields[-1] -*))  ((= variablelength array only at end =))
+  for (int i=0; i * sizeof( (((field|mk_id))) )  < msg_len; i++) {
+  ((*- endif -*))  ((= variable vs fixed =))
+    if (i != 0) json_bufp += snprintf(json_bufp, json_end - json_bufp, ",");
+  ((*- if (field|mk_id|is_simple) -*))
+    json_bufp += snprintf(json_bufp, json_end - json_bufp, "(((field|get_format_str)))", in->(((field.identifier)))[i]);
+  ((*- else -*))
+    json_bufp += (((field|mk_id)))_to_json_str(in->(((field.identifier))), json_end - json_bufp, json_bufp);
+  ((*- endif -*))
+  }
+  json_bufp += snprintf(json_bufp, json_end - json_bufp, "]");
   ((*- endif -*))
   ((*- endfor -*))
   json_bufp += snprintf(json_bufp, json_end - json_bufp, "}");
