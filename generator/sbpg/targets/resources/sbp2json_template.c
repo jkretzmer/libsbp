7402eaa82a68153ffbb991072e5ee5d9caee92f4
Josh Kretzmer <jkretzmer@swift-nav.com>
2017-12-04 18:19:31 -0800

    back to new logic, issue found

--- generator/sbpg/targets/resources/sbp2json_template.c
+++ generator/sbpg/targets/resources/sbp2json_template.c
@@ -34,67 +34,103 @@
 
 
 ((* for m in msgs *)) ((= Iterate over each message =))
-((*- if m.desc *))
-/** (((m.short_desc)))
- *
-(((m.desc|commentify)))
- */
-((*- endif *))
-
-((*- if m.sbp_id *))
-#define MSG_((('%04X'|format(m.sbp_id))))_TO_JSON (((m.identifier|convert)))_to_json_str
-((*- endif *))
 
 ((*- if m.fields *))
 ((*- set in_ptr_type=(((m.identifier|convert))) *))
 ((*- else *))
 ((*- set in_ptr_type="void" *))
 ((*- endif *))
-
-((* if m.sbp_id *))
+((*- if m.sbp_id -*))
+#define MSG_((('%04X'|format(m.sbp_id))))_TO_JSON (((m.identifier|convert)))_to_json_str
 ((= The ID determines for me whether this is an SBP message or a sub structure =))
 int (((m.identifier|convert)))_to_json_str( u16 sender_id, u16 msg_type, u8 msg_len, (((in_ptr_type))) * in, uint64_t max_len, char* out_str) {
   (void) sender_id;
   (void) msg_type;
   (void) msg_len;
-((* else -*))
+((*- else -*)) ((= if not a message I dont have any of the framing types =))
 int (((m.identifier|convert)))_to_json_str( (((in_ptr_type))) * in, uint64_t max_len, char* out_str) {
-((*- endif -*))
-
+((*- endif -*)) ((= end if m.sbp_id  =))
   (void) in;
   (void) out_str;
   (void) max_len;
+  ((= Simple type processing (done for brevity)  =))
   ((*- if (m|entirely_simple) and m.sbp_id *))
   return sprintf(out_str, (((m|mk_str_format_msg))), (((m|mk_arg_list_msg))));
   ((*- elif (m|entirely_simple) *))
   return sprintf(out_str, (((m|mk_str_format))), (((m|mk_arg_list))));
-  ((* else *))
+  ((*- else *))   ((= No Simple type=))
   char * const json_end = (char*) out_str + max_len;
   char * json_bufp = (char*) out_str;
   (void) json_end;
   (void) json_bufp;
-
-  ((*- if m.sbp_id *))
-  json_bufp += snprintf(out_str, json_end - json_bufp, "{\"msg_type\": %u, \"sender\":%u, \"length\":%u", msg_type, sender_id, msg_len);
+  ((*- if m.sbp_id -*)) ((= If message print out framing items=))
+  json_bufp += snprintf(out_str, json_end - json_bufp, "{\"msg_type\": %u, \"sender\": %u, \"length\": %u", msg_type, sender_id, msg_len);
+  ((*- else -*))
+  json_bufp += snprintf(out_str, json_end - json_bufp, "{");
+  ((*- endif -*))
+  ((= Separate Print for each field=))
+  ((*- for field in m.fields -*))
+  ((= simple type field =))
+  ((*- if (m.sbp_id or field != m.fields[0] ) -*))  ((= comma in between every field except first=))
+  json_bufp += snprintf(json_bufp, json_end - json_bufp, ", ");
   ((*- endif *))
-
-  ((*- for field in m.fields *))
-    ((*- if (field.type_id|is_simple) *))
-  json_bufp += snprintf(json_bufp, json_end - json_bufp, ", \"(((field.identifier)))\": (((field|get_format_str)))", in->(((field.identifier))));
-    ((*- elif (field.type_id != "array" and not field.type_id|is_string) *))
-  json_bufp += snprintf(json_bufp, json_end - json_bufp, ", \"(((field.identifier)))\":");
+  json_bufp += snprintf(json_bufp, json_end - json_bufp, "\"(((field.identifier)))\": ");
+  ((* if (field.type_id|is_simple) -*))
+  json_bufp += snprintf(json_bufp, json_end - json_bufp, "(((field|get_format_str)))", in->(((field.identifier))));
+  ((= Nested type field =))
+  ((*- elif (field.type_id != "array" and not field.type_id|is_string) -*))
   json_bufp += (((field.type_id|convert)))_to_json_str(&in->(((field.identifier))), json_end - json_bufp, json_bufp);
-    ((*- elif (field.type_id == "array" and field.options.get('size', None)) -*))
-  json_bufp += snprintf(json_bufp, json_end - json_bufp, ", \"(((field.identifier)))\": {");
+  ((= fixed legnth string (special type of fixed size array in our case) =))
+  ((*- elif (field|mk_id|is_string) and (field.options.get('size', None)) -*))
+  json_bufp += snprintf(json_bufp, json_end - json_bufp, "\"");
   for (int i=0; i < (((field.options.get('size').value))); i++) {
-      ((* if (field|mk_id|is_simple) *))
-    json_bufp += snprintf(json_bufp, json_end - json_bufp, "(((field|get_format_str)))", in->(((field.identifier)))[i]);
-      ((* else -*))
-    json_bufp += (((field|mk_id)))_to_json_str(in->(((field.identifier))), json_end - json_bufp, json_bufp);
-      ((* endif -*))
+    unsigned char c = in->(((field.identifier)))[i];
+    if (isprint(c) && c != '\\') {
+    json_bufp += snprintf(json_bufp, json_end - json_bufp, "%c", c);
     }
+    else {
+    json_bufp += snprintf(json_bufp, json_end - json_bufp, "\\\\u00%02x", c);
+    }
+  }
+  json_bufp += snprintf(json_bufp, json_end - json_bufp, "\"");
+  ((*- elif (field|mk_id|is_string and not (field.options.get('size', None))) -*))
+  json_bufp += snprintf(json_bufp, json_end - json_bufp, "\"");
+  for (int i=0; i < (uint8_t *) in + msg_len - (uint8_t *) &(in->(((field.identifier)))); i++) {
+    unsigned char c = in->(((field.identifier)))[i];
+    if (isprint(c) && c != '\\') {
+      json_bufp += snprintf(json_bufp, json_end - json_bufp, "%c", c);
+    }
+    else {
+      json_bufp += snprintf(json_bufp, json_end - json_bufp, "\\\\u00%02x", c);
+    }
+  }
+  json_bufp += snprintf(json_bufp, json_end - json_bufp, "\"");
+  ((*- elif (field.type_id == "array" ) -*))
+  json_bufp += snprintf(json_bufp, json_end - json_bufp, "[");
+  ((*- if field.options.get('size', None) -*))
+  ((=loop over fixed size array =))
+
+  for (int i=0; i < (((field.options.get('size').value))); i++) {
+
+  ((*- elif field == m.fields[-1] -*))  ((= variablelength array only at end =))
+  for (int i=0;  (unsigned int)((i+1) * sizeof( (((field|mk_id))) ) ) <= (uint8_t)((uint8_t *) (in + msg_len) - (uint8_t *) &(in->(((field.identifier))))); i++) {
+    ((*- endif -*))  ((= variable vs fixed =))
+    printf("i+1 * sizeof: %u\n", (unsigned int)((i+1) * sizeof( (((field|mk_id))) ) ) );
+    printf("array size: %d\n", (uint8_t)((uint8_t *) (in + msg_len) - (uint8_t *) &(in->(((field.identifier))))) );
+    if (i != 0){
+      json_bufp += snprintf(json_bufp, json_end - json_bufp, ", ");
+    }
+    ((* if (field|mk_id|is_simple) -*))
+    json_bufp += snprintf(json_bufp, json_end - json_bufp, "(((field|get_format_str)))", in->(((field.identifier)))[i]);
+
+    ((*- else -*))
+    json_bufp += (((field|mk_id)))_to_json_str(&(in->(((field.identifier)))[i]), json_end - json_bufp, json_bufp);
+
     ((*- endif -*))
-  ((* endfor *))
+  }
+  json_bufp += snprintf(json_bufp, json_end - json_bufp, "]");
+  ((*- endif -*))
+  ((*- endfor -*))
   json_bufp += snprintf(json_bufp, json_end - json_bufp, "}");
   return json_bufp - out_str;
   ((*- endif *))
@@ -102,6 +138,7 @@ int (((m.identifier|convert)))_to_json_str( (((in_ptr_type))) * in, uint64_t max
 ((* endfor *))
 ((* endwith *))
 ((* endfor *))
+
 
 int sbp2json(u16 sender_id, u16 msg_type, u8 msg_len,
                        u8 payload[], uint64_t max_len, char* out_str) {
